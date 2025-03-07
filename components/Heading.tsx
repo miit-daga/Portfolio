5028d0c4cfa442cd4916cdeb3f5360818a390ce3
Miit Daga <miitcodes27@gmail.com>
2025-03-07 05:40:02 +0530

    making heading responsive for smaller screens

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Heading = ({ text = "Projects" }) => {
   return (
-    <h1 className='py-10 text-center text-6xl font-extrabold lg:text-8xl'>
-        {text}
+    <h1 className='py-10 text-center text-4xl font-extrabold md:text-6xl lg:text-8xl'>
+      {text}
     </h1>
+
   )
 }
 
