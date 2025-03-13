12c6a2b0fdd5be3a575c8298707c81f4e66e47f4
Miit Daga <miitcodes27@gmail.com>
2025-03-13 11:23:35 +0530

    meta data changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,29 @@
+// import type { Metadata } from "next";
+// import { Outfit } from "next/font/google";
+// import "./globals.css";
+// import { Socials } from "@/components";
+
+// const outfit = Outfit({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: "Miit Daga",
+//   description: "Code that powers the unseen",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body className={outfit.className}>
+//         {children}
+//         <Socials />
+//       </body>
+//     </html>
+//   );
+// }
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
@@ -8,15 +34,21 @@ const outfit = Outfit({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Miit Daga",
   description: "Code that powers the unseen",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="description" content="Code that powers the unseen" />
+      </head>
       <body className={outfit.className}>
         {children}
         <Socials />
