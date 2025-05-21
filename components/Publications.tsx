12c32f99d2cc5caf56090f3ea5ea2d2188a84a93
Miit Daga <miitcodes27@gmail.com>
2025-05-21 10:43:52 +0530

    added link for publication

--- components/Publications.tsx
+++ components/Publications.tsx
@@ -62,14 +62,14 @@ import Heading from "@/components/Heading";
 const Publications = () => {
     const publications = [
         {
+            title: "DeepSeqCoco: A Robust Mobile Friendly Deep Learning Model for Detection of Diseases in Cocos nucifera",
+            description: "Proposes DeepSeqCoco, an EfficientNet-B3 based deep learning model for coconut disease detection, optimized for mobile use. Achieves high accuracy (up to 99.5%) with reduced computational cost compared to existing methods, tested using SGD, Adam, and hybrid optimizers. Accepted for publication in IEEE Access (pending revisions).",
+            link: "https://doi.org/10.1109/ACCESS.2025.3571800"
+        },
+        {
             title: "Enhancing Cognitive Evaluation: A Thorough Examination of the Hong Kong Adaptation of the Montreal Cognitive Assessment (HK-MoCA) and Bayesian Integration",
             description: "A detailed review of the Hong Kong adaptation of the Montreal Cognitive Assessment (HK-MoCA), exploring its improvements through the integration of Bayesian techniques to enhance cognitive evaluation accuracy. The work is published in a book with ISBN 978-93-6674-263-2.",
             link: "https://rb.gy/0qoeij"
-        },
-        {
-            title: "DeepSeqCoco: A Robust Mobile Friendly Deep Learning Model for Detection of Diseases in Cocos nucifera",
-            description: "Proposes DeepSeqCoco, an EfficientNet-B3 based deep learning model for coconut disease detection, optimized for mobile use. Achieves high accuracy (up to 99.5%) with reduced computational cost compared to existing methods, tested using SGD, Adam, and hybrid optimizers. Accepted for publication in IEEE Access (pending revisions).",
-            link: ""
         },
         {
             title: "Statistical Validation in Cultural Adaptations of Cognitive Tests: A Multi-Regional Systematic Review ",
